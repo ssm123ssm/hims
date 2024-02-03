5ac97049c55ad86b6eb610427efaff94374606e7
Supun Manathunga <ssm123ssm@gmail.com>
2024-02-03 16:58:14 +0530

    time-stamps

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -8,14 +8,23 @@ import {
   DropdownTrigger,
 } from "@nextui-org/react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
+import { Checkbox } from "@nextui-org/react";
 
 const Sidebar = () => {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   return (
-    <div className="h-full mr-4 flex  w-full">
-      <div className="flex justify-center items-start w-full">sidebar</div>
+    <div className="h-full mr-4 flex ">
+      <div className="flex justify-center items-start flex-col w-full gap-5 ml-5">
+        <Checkbox className="flex justify-center" lineThrough>
+          Form validation
+        </Checkbox>
+        <Checkbox lineThrough>Hx and Ex</Checkbox>
+        <Checkbox lineThrough>Edit past Ix</Checkbox>
+        <Checkbox lineThrough>Discharge card</Checkbox>
+        <Checkbox lineThrough>Fix Vercel cache issue</Checkbox>
+      </div>
     </div>
   );
 };
