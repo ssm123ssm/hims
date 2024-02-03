10afd27cd0dfd6b7aeee5ebd97b2481f56772907
Supun Manathunga <ssm123ssm@gmail.com>
2024-02-03 18:00:32 +0530

    Trend component fix

--- app/components/Trend.jsx
+++ app/components/Trend.jsx
@@ -17,9 +17,10 @@ const Trend = ({ props: { data, good } }) => {
     good === "high" ? "text-red-500 mx-2 mt-1" : "text-green-500 mx-2 mt-1";
   return (
     <>
-      {data.length > 1 && data.slice(-1)[0] > data.slice(-2)[0] ? (
+      {data.length > 1 && data.slice(-1)[0].value > data.slice(-2)[0].value ? (
         <FontAwesomeIcon icon={faArrowTrendUp} size="xs" className={up} />
-      ) : data.length > 1 && data.slice(-1)[0] < data.slice(-2)[0] ? (
+      ) : data.length > 1 &&
+        data.slice(-1)[0].value < data.slice(-2)[0].value ? (
         <FontAwesomeIcon icon={faArrowTrendDown} size="xs" className={down} />
       ) : (
         <></>
