41f971b416d3689e9d58a3d7dc62a7f280216edf
Supun Manathunga <ssm123ssm@gmail.com>
2024-02-05 22:02:45 +0530

    dissapearing btn in Ix Edit page

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -95,7 +95,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex my-2 mx-3 lg:ml-10 w-full justify-center md:justify-normal h-full overflow-auto">
+    <div className="mb-[80px] flex my-2 mx-3 lg:ml-10 w-full justify-center md:justify-normal h-full overflow-auto">
       <Scroller />
 
       {bedsLoaded ? (
