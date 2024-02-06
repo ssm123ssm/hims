788e6a9adc80f83d35de7cded2aa823f5fb49419
Supun Manathunga <ssm123ssm@gmail.com>
2024-02-06 06:48:20 +0530

    admission editor btn fix

--- app/components/Admission_card_editor.jsx
+++ app/components/Admission_card_editor.jsx
@@ -303,7 +303,7 @@ const Admission_card = ({ props: data }) => {
 
         <div
           className="
-              flex flex-row justify-around"
+              flex flex-row justify-around flex-wrap gap-3"
         >
           {submiting ? (
             <Button className="text-white bg-purple-400" isLoading isDisabled>
