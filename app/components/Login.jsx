f6e6e1d9283cf7ada04371ac1f2df6eede784db6
Supun Manathunga <ssm123ssm@gmail.com>
2024-02-17 11:33:09 +0530

    Login UI

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,12 +1,65 @@
+"use client";
+
 import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+  Button,
+} from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 const Login_component = () => {
+  const router = useRouter();
+
   return (
-    <div className="flex w-[90%] bg-slate-400 justify-center p-4">
-      <div className="flex flex-col justify-center">
-        <div>Welcome</div>
-        <div>Log in</div>
+    <div className="flex w-full justify-center p-4 text-white flex-col items-center gap-4 max-h-dvh min-h-[500px] mt-10">
+      <div className="flex flex-col items-center mb-5">
+        <div className="text-xl font-medium">Welcome Back</div>
+        <div className="tex-small text-default-500">
+          Please log in to continue
+        </div>
       </div>
+
+      <Card className="w-3/4 max-w-[400px]">
+        <CardBody>
+          <div className="flex gap-4 flex-col items-center">
+            <Input type="email" label="Email" placeholder="Enter your email" />
+            <Input
+              type="password"
+              label="Password"
+              placeholder="Enter the password"
+            />
+          </div>
+        </CardBody>
+      </Card>
+
+      <Button
+        color="primary"
+        size="md"
+        className="w-3/4 max-w-[400px]"
+        onPress={() => {
+          router.push("/");
+        }}
+      >
+        Log in
+      </Button>
+      <Divider orientation="horizontal" className="w-3/4 max-w-[400px]" />
+      <Button
+        color="primary"
+        variant="bordered"
+        className="w-3/4 max-w-[400px]"
+      >
+        SSO
+      </Button>
+      <p className="text-default-500 text-xs">
+        Need an acount? <Link className=" text-xs text-blue-500"> Sign up</Link>
+      </p>
     </div>
   );
 };
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,8 +3,11 @@ import Login_component from "@/app/components/Login";
 
 const Login = () => {
   return (
-    <div>
-      <Login_component />
+    <div className="justify-center items-center grid grid-cols-1 md:grid-cols-3">
+      <div className="col-span-1 md:col-span-3">
+        <Login_component />
+      </div>
+      \
     </div>
   );
 };
